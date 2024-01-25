6dea1525a1aa75c3122ffca8412b12761ceaf3a6
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2024-01-25 17:49:22 -0300

    REFACTOR: separacao em classe o comportamento do program

---
+++ Alura.Adopet.Console/Help.cs
@@ -0,0 +1,51 @@
+﻿namespace Alura.Adopet.Console
+{
+    public class Help
+    {
+        public void ExibeDocumentacao(string[] parametros)
+        {
+            System.Console.WriteLine("Lista de comandos.");
+
+            if (parametros.Length == 1)
+            {
+                AllCommands();
+            }
+            else if (parametros.Length == 2)
+            {
+                string comandoASerExibido = parametros[1];
+                if (comandoASerExibido.Equals("import"))
+                {
+                    Import();
+                }
+                if (comandoASerExibido.Equals("show"))
+                {
+                    Show();
+                }
+            }
+        }
+
+        private void AllCommands()
+        {      
+            System.Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
+                     "comando que exibe informações de ajuda dos comandos.");
+            System.Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
+            System.Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
+            System.Console.WriteLine("Comando possíveis: ");
+            System.Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
+            System.Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+            System.Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
+        }
+
+        private void Import()
+        {
+            System.Console.WriteLine(" adopet import <arquivo> " +
+                        "comando que realiza a importação do arquivo de pets.");
+        }
+
+        private void Show()
+        {
+            System.Console.WriteLine(" adopet show <arquivo>  comando que " +
+                        "exibe no terminal o conteúdo do arquivo importado.");
+        }
+    }
+}
---
+++ Alura.Adopet.Console/List.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Alura.Adopet.Console
+{
+    public class List
+    {
+        private readonly HttpClient _client;
+
+        public List()
+        {
+            _client = ConfiguraHttpClient("http://localhost:5057");
+        }
+
+        public async Task PetsAsync()
+        {
+            var pets = await ListPetsAsync();
+            foreach (var pet in pets)
+            {
+                System.Console.WriteLine(pet);
+            }
+        }
+
+        async Task<IEnumerable<Pet>?> ListPetsAsync()
+        {
+            HttpResponseMessage response = await _client.GetAsync("pet/list");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
+        }
+
+        HttpClient ConfiguraHttpClient(string url)
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri(url);
+            return _client;
+        }
+    }
+}
